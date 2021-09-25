f1b9aa6fa93ec569b62340c413f464274f6a69e2
Jim Sadler <jim@jsdevelopment.tech>
2021-09-25 15:36:33 -0400

    added db.json

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+
+    <v-main> <router-view /></v-main>
+  </v-app>
+</template>
+
+<script>
+import EventCard from './components/EventCard.vue'
+
+export default {
+  name: 'App'
+}
+</script>
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card :hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+h4 {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="events">
+    <h1>Events for Good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '../components/EventCard.vue'
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event',
+          location: 'atlanta',
+          date: 'January 28 2021',
+          time: '12:00 pm',
+          petsAllowed: true,
+          organizer: 'Jim Sadler'
+        },
+        {
+          id: 5928001,
+          category: 'animal welfare',
+          title: 'Dog Adoption Day',
+          description: 'Find your new canine friend at this event',
+          location: 'atlanta',
+          date: 'January 30 2022',
+          time: '12:00 pm',
+          petsAllowed: true,
+          organizer: 'Jim Sadler'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
